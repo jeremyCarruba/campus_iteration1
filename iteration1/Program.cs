93fd3692095b792a446f163e0f638b705ccde2b7
jeremyCarruba <jeremy.carruba@le-campus-numerique.fr>
2021-02-01 15:46:46 +0100

    g fé une class

--- iteration1/Program.cs
+++ iteration1/Program.cs
@@ -3,40 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using iteration1.Properties.Message;
 
 namespace iteration1
 {
     class Program
     {
 
-        
+
 
         static void Main(string[] args)
         {
-            var dateNow = DateTime.Now;
-            dateNow = new DateTime(2021, 02, 6, 22, 35, 0);
-            var day = dateNow.DayOfWeek;
-            var hour = dateNow.Hour;
-            String msg = null;
 
-            if (day != DayOfWeek.Sunday && day != DayOfWeek.Saturday)
-            {
-                 if (hour > 9 && hour < 13){
-                    msg = "Bonjour ";
-                }
-                else if (hour >= 13 && hour < 18) { 
-                    msg = "Bon après-midi ";
-                } else
-                {
-                    msg = "Bonsoir ";
-                }
-            } else
-            {
-                msg = "Bon week-end ";
-            }
-
-            Console.WriteLine("day: " + day + " hour: " + hour);
-            Console.WriteLine(msg + Environment.UserName + " the king de la route");
+            Message msg = new Message();
+            Console.WriteLine(msg.HelloMessage + Environment.UserName + " the king de la route");
         }
     }
 }
---
+++ iteration1/Properties/HelloMessage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iteration1.Properties.Message
+{
+    class Message
+    {
+        private string m_HelloMessage;
+        public string HelloMessage
+        {
+            get { return m_HelloMessage; }
+            set { m_HelloMessage = value; }
+        }
+
+        public Message()
+        {
+            var dateNow = DateTime.Now;
+            //dateNow = new DateTime(2021, 02, 6, 22, 35, 0);
+            var day = dateNow.DayOfWeek;
+            var hour = dateNow.Hour;
+           
+
+            if (day != DayOfWeek.Sunday && day != DayOfWeek.Saturday)
+            {
+                if (hour > 9 && hour < 13)
+                {
+                    this.HelloMessage = "Bonjour ";
+                }
+                else if (hour >= 13 && hour < 18)
+                {
+                    this.HelloMessage = "Bon après-midi ";
+                }
+                else
+                {
+                    this.HelloMessage = "Bonsoir ";
+                }
+            }
+            else
+            {
+                this.HelloMessage = "Bon week-end ";
+            }
+        }
+
+    }
+}
